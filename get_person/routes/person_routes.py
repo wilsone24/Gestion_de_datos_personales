cb2c62af20b98a0e6620201913e332653b2595b3
wilsone24 <wilsone@uninorte.edu.co>
2025-11-12 13:56:20 -0500

    Fix indentation and reformat get_person endpoint in person_routes

--- get_person/routes/person_routes.py
+++ get_person/routes/person_routes.py
@@ -10,11 +10,11 @@ from schemas.person_schema import PersonResponse
 router = APIRouter()
 
 
-@router.get("/get{document_number}", response_model=PersonResponse)
-def get_person(document_number: str, db: Session = Depends(get_db)):
-    return get_person_controller(db, document_number)
-
-
 @router.get("/getall", response_model=list[PersonResponse])
 def get_all_persons(db: Session = Depends(get_db)):
     return get_allp_persons_controller(db)
+
+
+@router.get("/get{document_number}", response_model=PersonResponse)
+def get_person(document_number: str, db: Session = Depends(get_db)):
+    return get_person_controller(db, document_number)

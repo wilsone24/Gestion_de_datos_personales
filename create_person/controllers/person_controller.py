eb0850c07d4a0df395e1fba830ae30a0e6c61113
wilsone24 <wilsone@uninorte.edu.co>
2025-11-23 11:44:15 -0500

    Refactor update_person_controller to create_person_controller for clarity and consistency

--- create_person/controllers/person_controller.py
+++ create_person/controllers/person_controller.py
@@ -4,7 +4,5 @@ from services import person_service
 from fastapi import UploadFile
 
 
-def update_person_controller(
-    db: Session, id_person: int, data: PersonRequest, photo: UploadFile | None
-):
-    return person_service.update_person(db, id_person, data, photo)
+def create_person_controller(db: Session, data: PersonRequest, photo: UploadFile | None):
+    return person_service.create_person(db, data, photo)

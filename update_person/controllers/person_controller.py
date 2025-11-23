1e72e76272d95b3e9d3d4a9625c23bf25f69aa22
wilsone24 <wilsone@uninorte.edu.co>
2025-11-23 11:33:17 -0500

    Refactor update_person_controller to directly import and use update_person from person_service

--- update_person/controllers/person_controller.py
+++ update_person/controllers/person_controller.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
 from schemas.person_schema import PersonRequest
-from services import person_service
+from services.person_service import update_person
 from fastapi import UploadFile
 
 
 def update_person_controller(
     db: Session, id_person: int, data: PersonRequest, photo: UploadFile | None
 ):
-    return person_service.update_person(db, id_person, data, photo)
+    return update_person(db, id_person, data, photo)
